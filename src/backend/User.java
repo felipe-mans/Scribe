ff3137ba47694178ca3ef595da6b93b259695163
DESKTOP-QGAI0FK\vince <vincentr@usc.edu>
2017-11-17 11:12:03 -0800

    added user class

---
+++ src/backend/User.java
@@ -0,0 +1,43 @@
+package backend;
+
+public class User {
+
+	private String mFirstname;
+	private String mLastname;
+	private String mUsername;
+	private String mPassword;
+	private String mEmail;
+
+	public User(String firstname, String lastname, String username, String password, String email) {
+		this.mFirstname = firstname;
+		this.mLastname = lastname;
+		this.mUsername = username;
+		this.mPassword = password;
+		this.mEmail = email;
+	}
+
+	public String getFirstname() {
+		return this.mFirstname;
+	}
+
+	public String getLastname() {
+		return this.mLastname;
+	}
+
+	public String getUsername() {
+		return this.mUsername;
+	}
+
+	public String getPassword() {
+		return this.mPassword;
+	}
+
+	public String getEmail() {
+		return this.mEmail;
+	}
+
+	public String getFullname() {
+		return this.getFirstname() + " " + this.getLastname();
+	}
+
+}
