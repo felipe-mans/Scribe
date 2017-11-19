ab64deb5a6c38e954e83aa5e583d991fbdec348d
DESKTOP-QGAI0FK\vince <vincentr@usc.edu>
2017-11-18 17:44:29 -0800

    sql script update

--- Scribe.sql
+++ Scribe.sql
@@ -2,7 +2,7 @@ DROP DATABASE if exists Scribe;
 CREATE DATABASE Scribe;
 USE Scribe;
 
-CREATE TABLE Users {
+CREATE TABLE Users (
 
 	userID INTEGER PRIMARY KEY not null auto_increment,
     fname VARCHAR(255) not null,
@@ -10,41 +10,41 @@ CREATE TABLE Users {
 	username VARCHAR(255) not null,
     password VARCHAR(255) not null,
     email VARCHAR(255)
-    };
+    );
 
-CREATE TABLE Classes {
+CREATE TABLE Classes (
 
 	classID INTEGER PRIMARY KEY not null auto_increment,
     classname VARCHAR(255) not null,
     private BOOLEAN not null,
-	};
+	);
 	
-CREATE TABLE Documents {
+CREATE TABLE Documents (
 
 	docID INTEGER PRIMARY KEY not null auto_increment,
     userID INTEGER not null,
     documentname VARCHAR(255) not null,
     file LONGBLOB not null
 	
-	};
+);
 	
-CREATE TABLE Enrollment {
+CREATE TABLE Enrollment (
 
 	enrollID INTEGER PRIMARY KEY not null auto_increment,
     classID INTEGER not null,
     userID INTEGER not null
 
-};
+);
 
-CREATE TABLE Uploads {
+CREATE TABLE Uploads (
 
 	uploadID INTEGER PRIMARY KEY not null auto_increment,
     classID INTEGER not null,
-    docID not null
+    docID INTEGER not null
     
-};
+);
 
-CREATE TABLE Messages {
+CREATE TABLE Messages (
 	
 	messageID INTEGER PRIMARY KEY not null auto_increment,
 	classID INTEGER not null,
@@ -52,7 +52,7 @@ CREATE TABLE Messages {
 	level INTEGER not null,
 	content VARCHAR(255) not null
 
-};
+);
 
 
 INSERT INTO Users (fname, lname, username, password, email)
