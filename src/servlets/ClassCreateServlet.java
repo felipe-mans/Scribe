0294f2d274c6931e7d20e4c8d107081db11154cc
noahfeldman1 <noahfeldman1@noahs-mbp-2>
2017-11-19 12:25:31 -0800

    add classcreateservlet

---
+++ src/servlets/ClassCreateServlet.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import backend.JDBCQuery;
+
+@WebServlet("/LoginServlet")
+public class ClassCreateServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	public ClassCreateServlet() {
+        super();
+    }
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String classname = request.getParameter("classname");
+		boolean isPrivate = Boolean.parseBoolean(request.getParameter("isPrivate"));
+		
+		if (classname == null){
+			request.setAttribute("error", "Error in ClassCreateServ: classname para null");
+			request.getRequestDispatcher("jsp/memberpage.jsp").forward(request, response);
+			return;
+		} else if (classname.equals("")){
+			request.setAttribute("error", "Field cannot be empty");
+			request.getRequestDispatcher("jsp/memberpage.jsp").forward(request, response);
+			return;
+		}
+		
+		JDBCQuery jdbcq = new JDBCQuery();
+		jdbcq.connect();
+		if(jdbcq.doesClassExist(classname)){
+			jdbcq.stop();
+			request.setAttribute("error", "Class already exists");
+			request.getRequestDispatcher("jsp/memberpage.jsp").forward(request, response);
+		}
+		else{
+			jdbcq.addClass(classname, isPrivate);
+			jdbcq.stop();
+		}
+	}
+}
